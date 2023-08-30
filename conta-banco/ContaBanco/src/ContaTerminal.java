cc2b651bdd80633d77aa893f76ef8006a5302a17
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2023-08-30 20:31:36 -0300

    Inserindo a classe Scanner e guardando os valores

--- conta-banco/ContaBanco/src/ContaTerminal.java
+++ conta-banco/ContaBanco/src/ContaTerminal.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //Atributos
-        int numero;
-        String agencia;
-        String nomeCliente;
-        double saldo;
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+        String agencia = scanner.next();
+        String nomeCliente = scanner.next();
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
     }
 }
