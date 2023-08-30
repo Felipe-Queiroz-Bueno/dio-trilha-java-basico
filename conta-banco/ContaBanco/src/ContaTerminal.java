751f8a19f7128655b979b8942711e3fe94278c29
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2023-08-30 20:20:54 -0300

    Inserindo atributo conforme os requisitos
    Inserindo atributo conforme os requisitos:
    https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe

--- conta-banco/ContaBanco/src/ContaTerminal.java
+++ conta-banco/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,9 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Atributos
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
     }
 }
